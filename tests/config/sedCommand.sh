97dffed8c2d472fb8b164e108de04626765e48d6
alevento <a@a.it>
2016-09-20 16:57:12 +0200

    Substitute

--- tests/config/sedCommand.sh
+++ tests/config/sedCommand.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i 's/"psr-4": {/"psr-4": { "@@@namespacevendor\\\\@@@namespacepackage_name\\\\Test\\\\": ".\/vendor\/@@@vendor\/@@@package_name\/tests\/",/g' ./composer.json
+sed -i 's/"psr-4": {/"psr-4": { "Padosoft\\\\Gtk\\\\Test\\\\": ".\/vendor\/padosoft\/gtk\/tests\/",/g' ./composer.json
